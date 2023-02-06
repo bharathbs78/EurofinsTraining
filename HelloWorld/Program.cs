1e7149cbdc7eab32cd475d4012a3f05cc0315912
Admin <Admin@DESKTOP-VU3I9MN>
2023-02-06 16:05:40 +0530

    Add project files.

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    internal class Program
+    {
+        public string PersonName;//pascal case, public
+        static void Main(string[] args)
+        {
+            string personName;//camel case, private and local
+            Console.WriteLine("Hello World");
+        }
+    }
+}
